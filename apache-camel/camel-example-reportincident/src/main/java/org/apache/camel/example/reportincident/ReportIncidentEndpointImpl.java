73d87dbba6df9a99c88fb64026e6943bad34d4fc
Jackson Arias <Jackson Arias>
2017-03-27 18:43:40 -0500

    ReportIncident endpoint and reportIncident test case added

---
+++ apache-camel/camel-example-reportincident/src/main/java/org/apache/camel/example/reportincident/ReportIncidentEndpointImpl.java
@@ -0,0 +1,16 @@
+package org.apache.camel.example.reportincident;
+
+/**
+ * The webservice we have implemented.
+ */
+public class ReportIncidentEndpointImpl implements ReportIncidentEndpoint{
+
+	public OutputReportIncident reportIncident(InputReportIncident parameters) {
+		System.out.println("Hello ReportIncidentEndpointImpl is called from " + parameters.getGivenName());
+		 
+        OutputReportIncident out = new OutputReportIncident();
+        out.setCode("OK");
+        return out;
+	}
+
+}
