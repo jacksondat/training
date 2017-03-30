5d4fa295ae2f78f7e4da0ab8b97599c9166a4bcf
Jackson Arias <Jackson Arias>
2017-03-29 23:11:34 -0500

    added Mail Body construct functionality with Camel-Velocity endpoint

--- apache-camel/camel-example-reportincident/src/main/java/org/apache/camel/example/reportincident/ReportIncidentEndpointImpl.java
+++ apache-camel/camel-example-reportincident/src/main/java/org/apache/camel/example/reportincident/ReportIncidentEndpointImpl.java
@@ -1,16 +1,8 @@
 package org.apache.camel.example.reportincident;
 
-import java.io.File;
-
 import org.apache.camel.CamelContext;
-import org.apache.camel.Component;
-import org.apache.camel.Endpoint;
-import org.apache.camel.Exchange;
-import org.apache.camel.Producer;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.component.file.FileComponent;
-import org.apache.camel.component.file.FileEndpoint;
-import org.apache.camel.component.log.LogComponent;
 import org.apache.camel.impl.DefaultCamelContext;
 
 /**
@@ -28,7 +20,6 @@ public class ReportIncidentEndpointImpl implements ReportIncidentEndpoint{
 	    // get the ProducerTemplate thst is a Spring'ish xxxTemplate based producer for very
 	    // easy sending exchanges to Camel.
 	    template = camel.createProducerTemplate();
-
 	 
 	    // start Camel
 	    camel.start();
@@ -37,9 +28,15 @@ public class ReportIncidentEndpointImpl implements ReportIncidentEndpoint{
 	public OutputReportIncident reportIncident(InputReportIncident parameters) {
 		String name = parameters.getGivenName() + " " + parameters.getFamilyName();
 	    
-	    template.sendBody("log:com.mycompany.part2.easy", name);
-	    String filename = "easy-incident-" + parameters.getIncidentId() + ".txt";
-	    template.sendBodyAndHeader("file://target/subfolder", name, FileComponent.HEADER_FILE_NAME, filename);
+	    String filename = "mail-incident-" + parameters.getIncidentId() + ".txt";
+	    
+	    // generate the mail body using velocity template
+	    // notice that we just pass in our POJO (= InputReportIncident) that we
+	    // got from Apache CXF to Velocity.
+	    Object mailBody = template.sendBody("velocity:mailBody.vm", parameters);
+	    // Note: the response is a String and can be cast to String if needed
+	    
+	    template.sendBodyAndHeader("file://target/subfolder", mailBody, FileComponent.HEADER_FILE_NAME, filename);
 		
         OutputReportIncident out = new OutputReportIncident();
         out.setCode("OK");
