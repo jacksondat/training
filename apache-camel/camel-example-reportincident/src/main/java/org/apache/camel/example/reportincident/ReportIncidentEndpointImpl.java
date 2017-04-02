e284dccf54132adecb045474b4fb0823bfd5a4d8
Jackson Arias <Jackson Arias>
2017-04-02 12:48:37 -0500

    Test for Mail Consumer Added

--- apache-camel/camel-example-reportincident/src/main/java/org/apache/camel/example/reportincident/ReportIncidentEndpointImpl.java
+++ apache-camel/camel-example-reportincident/src/main/java/org/apache/camel/example/reportincident/ReportIncidentEndpointImpl.java
@@ -1,6 +1,10 @@
 package org.apache.camel.example.reportincident;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.Consumer;
+import org.apache.camel.Endpoint;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.component.file.FileComponent;
 import org.apache.camel.impl.DefaultCamelContext;
@@ -14,12 +18,25 @@ public class ReportIncidentEndpointImpl implements ReportIncidentEndpoint{
 	private ProducerTemplate template;
 	
 	public ReportIncidentEndpointImpl() throws Exception {
+		init(true);
+	}
+	
+	public ReportIncidentEndpointImpl(boolean enableMailConsumer) throws Exception {
+		init(enableMailConsumer);
+	}
+	
+	public void init(boolean enableMailConsumer) throws Exception {
 	    // create the camel context that is the "heart" of Camel
 	    camel = new DefaultCamelContext();
 	    
 	    // get the ProducerTemplate thst is a Spring'ish xxxTemplate based producer for very
 	    // easy sending exchanges to Camel.
 	    template = camel.createProducerTemplate();
+	    
+	    if(enableMailConsumer){
+		    // add the event driven consumer that will listen for mail files and process them
+		    addMailSendConsumer();
+	    }
 	 
 	    // start Camel
 	    camel.start();
@@ -41,6 +58,36 @@ public class ReportIncidentEndpointImpl implements ReportIncidentEndpoint{
         OutputReportIncident out = new OutputReportIncident();
         out.setCode("OK");
         return out;
+	}
+	
+	private void addMailSendConsumer() throws Exception {
+	    // Grab the endpoint where we should consume. Option - the first poll starts after 2 seconds
+	    Endpoint endpint = camel.getEndpoint("file://target/subfolder?consumer.initialDelay=2000");
+	 
+	    // create the event driven consumer
+	    // the Processor is the code what should happen when there is an event
+	    // (think it as the onMessage method)
+	    Consumer consumer = endpint.createConsumer(new Processor() {
+	        public void process(Exchange exchange) throws Exception {
+	            // get the mail body as a String
+	            String mailBody = exchange.getIn().getBody(String.class);
+	            sendEmail(mailBody);
+	            System.out.println("Email sent");
+	        }
+	    });
+	 
+	    // star the consumer, it will listen for files
+	    consumer.start();
+	}
+	
+	private void sendEmail(String mailBody){
+		// send the email to your mail server
+	    String url = "smtp://someone@localhost?password=secret&to=jacksondat@gmail.com";
+	    template.sendBodyAndHeader(url, mailBody, "subject", "New incident reported");
+	}
+
+	public ProducerTemplate getTemplate() {
+		return template;
 	}
 	
 }
--- apache-camel/camel-example-reportincident/src/test/java/org/apache/camel/example/reportincident/ReportIncidentEndpointTest.java
+++ apache-camel/camel-example-reportincident/src/test/java/org/apache/camel/example/reportincident/ReportIncidentEndpointTest.java
@@ -2,18 +2,21 @@ package org.apache.camel.example.reportincident;
 
 import javax.xml.ws.Endpoint;
 
+import org.apache.camel.component.file.FileComponent;
 import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
 import org.junit.Test;
+import org.jvnet.mock_javamail.Mailbox;
 
 import junit.framework.TestCase;
 
 public class ReportIncidentEndpointTest extends TestCase {
 	private static String ADDRESS = "http://localhost:9090/unittest";
+	private ReportIncidentEndpointImpl endpoint;
 	 
 	protected void startServer() throws Exception {
 	    // We need to start a server that exposes or webservice during the unit testing
 	    // We use jaxws to do this pretty simple
-	    ReportIncidentEndpointImpl server = new ReportIncidentEndpointImpl();
+		ReportIncidentEndpoint server = new ReportIncidentEndpointImpl(false);
 	    Endpoint.publish(ADDRESS, server);
 	}
 	
@@ -43,6 +46,30 @@ public class ReportIncidentEndpointTest extends TestCase {
 	 
 	    OutputReportIncident out = client.reportIncident(input);
 	    assertEquals("Response code is wrong", "OK", out.getCode());
+	}
+	
+	@Test
+	public void testReportIncidentSendMail() throws Exception{
+		// we run this unit test with the consumer, hence the true parameter
+        endpoint = new ReportIncidentEndpointImpl(true);
+ 
+        // get the mailbox
+        Mailbox box = Mailbox.get("jacksondat@gmail.com");
+        assertEquals("Should not have mails", 0, box.size());
+ 
+        // drop a file in the folder that the consumer listen
+        // here is a trick to reuse Camel! so we get the producer template and just
+        // fire a message that will create the file for us
+        endpoint.getTemplate().sendBodyAndHeader("file://target/subfolder?append=false", "Hello World",
+            FileComponent.HEADER_FILE_NAME, "mail-incident-test.txt");
+ 
+        // let the consumer have time to run
+        Thread.sleep(3 * 1000);
+ 
+        // get the mock mailbox and check if we got mail ;)
+        assertEquals("Should have got 1 mail", 1, box.size());
+        assertEquals("Subject wrong", "New incident reported", box.get(0).getSubject());
+        assertEquals("Mail body wrong", "Hello World", box.get(0).getContent());
 	}
 	
 }
